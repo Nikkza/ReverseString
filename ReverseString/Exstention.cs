76a47fbdaa76c8c55239a81dc3561b0653283bc1
Nikkza <isaksson.nicklas80@outlook.com>
2019-09-26 21:45:08 +0300

    Add project files.

---
+++ ReverseString/Exstention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    public static class Extension
+    {
+        public static bool CheckforNumbers(this string value)
+        {
+            bool checkString = false;
+            checkString = value.Any(char.IsDigit);
+
+            return checkString;
+        }
+    }
+}
---
+++ ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string value = Console.ReadLine();
+                if (value.ToLower() == "q") break;
+                if (Extension.CheckforNumbers(value))
+                    Console.WriteLine("Value can only be from A-Ö");
+                else
+                {
+                    string result = RevereString(value);
+                    Console.WriteLine($"REVERS STRING: {result} ");
+                }
+            }
+        }
+
+        static string RevereString(string value)
+        {
+            string result = string.Join(" ", value
+                .Split(' ')
+                .Select(x => new String(x.Reverse().ToArray())));
+
+            return result;
+        }
+    }
+}
